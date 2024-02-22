80d920f26433ccb887c93ac50f63a2cee6ad83d0
washucode <estherw8525@gmail.com>
2024-02-22 07:57:12 +0300

    task 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -40,3 +40,16 @@ class Server:
 
         start, end = index_range(page, page_size)
         return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Return a dictionary containing the following key-value pairs"""
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages
+        }
