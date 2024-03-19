11fe00c58af038ac5bf3f7af45f873558d680f50
washucode <estherw8525@gmail.com>
2024-03-19 14:28:02 +0300

    align with alx

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -20,14 +20,15 @@ class Config:
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
+babel = Babel(app)
 
-
+@babel.localeselector
 def get_locale():
     """Get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel = Babel(app, locale_selector=get_locale)
+# babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/')
--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -21,7 +21,10 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 
+babel = Babel(app)
 
+
+@babel.localeselector
 def get_locale():
     """Get locale"""
     locale = request.args.get('locale')
@@ -29,7 +32,8 @@ def get_locale():
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-babel = Babel(app, locale_selector=get_locale)
+
+#babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/')
