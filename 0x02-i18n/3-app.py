046a05272a48f2eb5c3855ff092bbec00d6e275e
washucode <estherw8525@gmail.com>
2024-03-19 14:17:17 +0300

    try

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -21,13 +21,13 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 
-babel = Babel(app)
 
-
-@babel.localeselector
 def get_locale():
     """Get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/')
--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -21,16 +21,15 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 
-babel = Babel(app)
 
-
-@babel.localeselector
 def get_locale():
     """Get locale"""
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/')
